e576d3498016f1e3b374f932dda444374deb11fb
HasanKaptan <hsinangh@gmail.com>
2020-02-04 16:40:04 -0500

    new project

---
+++ src/test/java/com/hrms/steps/LoginSteps.java
@@ -0,0 +1,71 @@
+package com.hrms.steps;
+
+import org.junit.Assert;
+
+import com.hrms.pages.LoginPageElements;
+import com.hrms.utils.CommonMethods;
+
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+public class LoginSteps extends CommonMethods{
+
+	LoginPageElements login;
+	
+	@Given("I open browser and navigated to the HRMS")
+	public void i_open_browser_and_navigated_to_the_HRMS() {
+	   setUp();
+	}
+
+	@When("I enter valid username and valid password")
+	public void i_enter_valid_username_and_valid_password() {
+	    login=new LoginPageElements();
+		sendText(login.username, "admin");
+		sendText(login.password, "Syntax@123");
+	}
+
+	@When("I click on login button")
+	public void i_click_on_login_button() {
+	   click(login.loginBtn);
+	}
+
+	@Then("I successfully logged in")
+	public void i_successfully_logged_in() {
+	    System.out.println("I am logged in");
+	}
+	
+	@When("I enter valid username and invalid password")
+	public void i_enter_valid_username_and_invalid_password() {
+	    login=new LoginPageElements();
+		sendText(login.username, "Admin");
+	    sendText(login.password, "66jhhj");
+	}
+
+	@Then("I see error message")
+	public void i_see_error_message() {
+	   boolean error=login.errorMsg.isDisplayed();
+	   Assert.assertTrue("Error message is not displayed", error);
+	}
+	@When("I enter {string} and {string}")
+	public void i_enter_and(String Username, String Password) {
+	    login=new LoginPageElements();
+	    sendText(login.username,Username);
+	    sendText(login.password,Password);
+	}
+
+	@Then("I see {string}")
+	public void i_see(String ErrorMessage) {
+	    login=new LoginPageElements();
+		String actualErrMessage=login.errorMsg.getText();
+		Assert.assertEquals("error message not matched", ErrorMessage, actualErrMessage);
+	}
+
+	
+	
+	
+	@Then("I close browser")
+	public void i_close_browser() {
+	    tearDown();
+	}
+}
---
+++ target/html/cucumber-default-report/report.js
@@ -0,0 +1,78 @@
+$(document).ready(function() {var formatter = new CucumberHTML.DOMFormatter($('.cucumber-report'));formatter.uri("file:src/test/resources/features/Login.feature");
+formatter.feature({
+  "name": "Login",
+  "description": "",
+  "keyword": "Feature",
+  "tags": [
+    {
+      "name": "@sprint1"
+    },
+    {
+      "name": "@login"
+    }
+  ]
+});
+formatter.scenario({
+  "name": "Valid login",
+  "description": "",
+  "keyword": "Scenario",
+  "tags": [
+    {
+      "name": "@sprint1"
+    },
+    {
+      "name": "@login"
+    },
+    {
+      "name": "@smoke"
+    }
+  ]
+});
+formatter.before({
+  "status": "passed"
+});
+formatter.step({
+  "name": "I open browser and navigated to the HRMS",
+  "keyword": "Given "
+});
+formatter.match({
+  "location": "LoginSteps.i_open_browser_and_navigated_to_the_HRMS()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "I enter valid username and valid password",
+  "keyword": "When "
+});
+formatter.match({
+  "location": "LoginSteps.i_enter_valid_username_and_valid_password()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "I click on login button",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "LoginSteps.i_click_on_login_button()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "I successfully logged in",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "LoginSteps.i_successfully_logged_in()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.embedding("image/png", "embedded0.png");
+formatter.after({
+  "status": "passed"
+});
+});
